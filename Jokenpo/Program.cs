f7da1bc8cc9db37002375da53d299270de78213c
Enzo Monteiro <enzovila.monteiro@gmail.com>
2026-01-28 01:07:13 -0300

    feat: Adding swagger

--- Jokenpo/Program.cs
+++ Jokenpo/Program.cs
@@ -12,12 +12,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "Swagger"));
 }
 
 app.UseHttpsRedirection();
 
 app.MapControllers();
-
 
 
 app.Run();
