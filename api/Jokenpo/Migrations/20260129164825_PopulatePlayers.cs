12f60c355b05565b3f0e3d8b2ffda1dd554e74e1
Enzo Monteiro <enzovila.monteiro@gmail.com>
2026-01-29 13:53:18 -0300

    feat: Populating user migration

---
+++ api/Jokenpo/Migrations/20260129164825_PopulatePlayers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jokenpo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulatePlayers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO \"Players\" (\"Id\", \"Name\") VALUES (1,'Test One')");
+            migrationBuilder.Sql("INSERT INTO \"Players\" (\"Id\", \"Name\") VALUES (2,'Test Two')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"Players\"");
+        }
+    }
+}
